b8cc17b803a702ec386e30e0152b25ad4c1575df
Daniel <danieleduardocald@gmail.com>
2021-02-01 21:48:42 -0500

    commit tree 3b1f1e498ffd91edc36aeb49e748cdd024be7b24

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function  writeCards(namesArray, event) {
+    
+    let array = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+        array.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return array
+
+};
+
+function countDown(n) {
+    for (let count = n; count >= 0; count --) {
+        console.log (count);
+    }
+};
+
+
